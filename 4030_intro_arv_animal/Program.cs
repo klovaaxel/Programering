524d890ce6f9bdaa59d1cac1f03a7d39de729e14
klovaaxel <klovakarlsson@gmail.com>
2020-09-11 13:26:31 +0200

    4030 dogs and cat class

--- 4030_intro_arv_animal/Program.cs
+++ 4030_intro_arv_animal/Program.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _4030_intro_arv_animal
-{
-    class Program
+namespace _4030_intro_arv_animal{
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            List<String> stringList = new List<string>;  
-        }
+      Cat minKatt = new Cat(5);
+      minKatt.Act();
     }
+
+  }
+
+  class Cat
+  {
+    private int age {get;}
+
+    public Cat(int publicAge)
+    {
+      age = publicAge;
+    }
+
+    public void Act()
+    {
+      for (int i = 0; i < age; i++)
+      {
+            Console.Write("mjau, ");
+      }
+    }
+  }
+
+  class Dog
+  {
+    private int age {get;}
+
+    public Dog(int publicAge)
+    {
+      age = publicAge;
+    }
+
+    public void Act()
+    {
+      for (int i = 0; i < age; i++)
+      {
+        Console.Write("vov, ");
+      }
+    }
+  }
+
+  class Zoo
+  {
+    private Object cats;
+    private Object dogs;
+
+    public Zoo()
+    {
+      
+    }  
+  }   
 }
