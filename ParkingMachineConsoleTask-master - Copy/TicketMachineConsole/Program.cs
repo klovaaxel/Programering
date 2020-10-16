b5c7012398f07c17730383088d6e33b61630f865
klovaaxel <klovakarlsson@gmail.com>
2020-10-16 07:32:28 -0700

    Update Program.cs

--- ParkingMachineConsoleTask-master - Copy/TicketMachineConsole/Program.cs
+++ ParkingMachineConsoleTask-master - Copy/TicketMachineConsole/Program.cs
@@ -6,17 +6,6 @@ namespace ParkingMachineConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            ParkingMachine machine = new ParkingMachine(10);
-
-            // Act
-            machine.InsertMoney(10);
-            machine.BuyTicket();
-            machine.InsertMoney(40);
-            machine.BuyTicket();
-
-            Console.Write("hej");
 
         }
     }
