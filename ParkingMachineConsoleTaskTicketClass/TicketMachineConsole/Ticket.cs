7d806656b8e5d47d05d867efe4c8c048d2138ac5
klovaaxel <klovakarlsson@gmail.com>
2020-10-23 04:26:48 -0700

    Update Ticket.cs

--- ParkingMachineConsoleTaskTicketClass/TicketMachineConsole/Ticket.cs
+++ ParkingMachineConsoleTaskTicketClass/TicketMachineConsole/Ticket.cs
@@ -26,18 +26,20 @@ namespace ParkingMachineConsole
         public TimeSpan GetValidTimeSpan() 
         {
             int days = Convert.ToInt32((currentTotal / costPerHour) / 24);
-            int Hours = Convert.ToInt32((currentTotal / costPerHour) % 24);
-            int Minutes = Convert.ToInt32((currentTotal * 60 / costPerHour) % 60);
-            return new TimeSpan(days: days, hours: 1, minutes: 0);
+            int hours = Convert.ToInt32((currentTotal / costPerHour) % 24);
+            int minutes = Convert.ToInt32((currentTotal * 60 / costPerHour) % 60);
+            return new TimeSpan(days: days, hours: hours, minutes: minutes, seconds: 0);
         }
         public DateTime GetValidTo() 
-        { 
-        
+        {
+            DateTime date = DateTime.Now;
+            date = date.Add(GetValidTimeSpan());
+            return date;
         }
 
         public String ToString() 
         { 
-        
+            
         }
     }
 }
