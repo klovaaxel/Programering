6b622482aa7378947b7ae630df4b0c10e3850dcd
klovaaxel <klovakarlsson@gmail.com>
2020-11-30 01:48:53 -0800

    Console app
    bankaccmenu and depositmoneymenu

--- ParkingMachineBankCoinParkingTask - Copy/Console/Program.cs
+++ ParkingMachineBankCoinParkingTask - Copy/Console/Program.cs
@@ -8,32 +8,97 @@ namespace ConsoleApp
 {
     class Program
     {
+        static Bank bank = new Bank();
+        static String accPin;
+        static String accountNr;
+        static String verAccPin;
+
         static void Main(string[] args)
         {
-            Bank bank = new Bank();
-            CoinParkingMachine coinParkingMachine = new CoinParkingMachine(30);
-            CardParkingMachine cardParkingMachine = new CardParkingMachine(30, bank, );
-            PrintMainMenu();
+        int costPerHour = 30;
+        String machinePin = "machine";
+        String machineAccountNr = bank.AddAccount(machinePin);
+        CoinParkingMachine coinParkingMachine = new CoinParkingMachine(costPerHour);
+        CardParkingMachine cardParkingMachine = new CardParkingMachine(costPerHour, bank, machineAccountNr);
+
+        PrintMainMenu();
 
         }
-
         static void PrintMainMenu()
         {
+            Console.Clear();
+
             Console.WriteLine("press 1 to make new bank account");
             Console.WriteLine("press 2 to deposit money into existing account");
             Console.WriteLine("press 3 to buy ticket with bank");
             Console.WriteLine("press 4 to buy ticket with coins");
 
             string answer = Console.ReadLine();
-            if (answer == "1") 
+            if (answer == "1")
             {
-                PrintMainMenu();
-            } 
+                PrintNewBankAccMenu();
+            }
+            else if (answer == "2")
+            {
+                PrintDepositMoneyMenu();
+            }
+
         }
 
-        static void PrintBankMenu()
+        static void PrintNewBankAccMenu()
         {
-            Console.WriteLine("");
+            Console.Clear();
+
+            Console.WriteLine("Make a new Bank account");
+
+            Console.WriteLine("Enter the wanted pin for your bank account");
+            accPin = Console.ReadLine();
+            Console.WriteLine("Enter pin again to verify");
+            verAccPin = Console.ReadLine();
+
+            if (accPin != verAccPin)
+            {
+                Console.WriteLine("Enter pin again to verify");
+                verAccPin = Console.ReadLine();
+            }
+
+            accountNr = bank.AddAccount(accPin);
+
+            Console.WriteLine("Your account number is " + accountNr);
+
+            NextMenu();
+            PrintMainMenu();
+        }
+
+        static void PrintDepositMoneyMenu() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter balance you want to insert to account");
+            Transfer transfer = new Transfer("1001", accountNr, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter pin to verify");
+            verAccPin = Console.ReadLine();
+
+            while (accPin != verAccPin)
+            {
+                Console.WriteLine("Pin does not match, try again");
+                verAccPin = Console.ReadLine();
+            }
+            Console.WriteLine("Pin verified");
+
+            bank.Transfer(transfer, verAccPin);
+
+            Console.WriteLine("Added balance to account : " + accountNr);
+
+            NextMenu();
+            PrintMainMenu();
+            
+        }
+
+        static void NextMenu() 
+        {
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
         }
     }
 }
