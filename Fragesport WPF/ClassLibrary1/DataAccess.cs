54a1668bbdc77f38927e992308b8a9fc455706e6
klovaaxel <klovakarlsson@gmail.com>
2020-12-21 01:42:08 -0800

    Start of DB
    Error in DataAccess line 13, suuggests i have no using system, i have, tried clearing nuget cache and reinstall nuget packages, rebuilding soultions

---
+++ Fragesport WPF/ClassLibrary1/DataAccess.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System.IO;
+using Windows.Storage;
+using System;
+
+namespace DataAccessLibrary
+{
+    public class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
--- Fragesport WPF/Fragesport File/Program.cs
+++ Fragesport WPF/Fragesport File/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using DataAccessLibrary;
 
 namespace Fragesport_File
 {
@@ -25,45 +27,17 @@ namespace Fragesport_File
              Console.WriteLine("your score is " + myScore.GetScore() + " / " + myScore.GetMaxScore());
              */
 
-            Console.WriteLine("Getting Connection ...");
-
-            var datasource = @"DESKTOP-PC\SQLEXPRESS";//your server
-            var database = "Students"; //your database name
-            var username = "sa"; //username of server to connect
-            var password = "password"; //password
-
-            //your connection string 
-            string connString = @"Data Source=" + datasource + ";Initial Catalog="
-                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
-
-            //create instanace of database connection
-            SqlConnection conn = new SqlConnection(connString);
 
 
-            try
-            {
-                Console.WriteLine("Openning Connection ...");
-
-                //open connection
-                conn.Open();
-
-                Console.WriteLine("Connection successful!");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: " + e.Message);
-            }
-
-            Console.Read();
-        }
-    }
+        }    
         public void start() 
         {
             FileReader File = new FileReader();
             File.ReadFromFile(questions);
+            DataAccess.InitializeDatabase();
         }
-        public void insertDB(List<QuestionCard> questions) 
-        { 
+        public void StartDB(List<QuestionCard> questions) 
+        {
             
         }
         private void AskQuestion(QuestionCard Q, Score s)
